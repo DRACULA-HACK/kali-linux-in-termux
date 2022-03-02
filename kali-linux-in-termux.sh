a1bcac68fdf5044b69c26d68a13c1dde80faaf34
DRACULA-HACK <96709855+DRACULA-HACK@users.noreply.github.com>
2022-03-02 23:28:18 +0530

    kali-linux-in-termux.sh

---
+++ kali-linux-in-termux.sh
@@ -0,0 +1,22 @@
+E325: ATTENTION
+Found a swap file by the name "~/TBomb2.0/.TBomb2.0.sh.swp"
+          owned by: kali   dated: Wed Mar 02 17:49:47 2022
+         file name: ~kali/TBomb2.0/TBomb2.0.sh
+          modified: no
+         user name: kali   host name: localhost
+        process ID: 19710 (STILL RUNNING)
+While opening file "/home/kali/TBomb2.0/TBomb2.0.sh"
+             dated: Wed Mar 02 10:54:37 2022
+
+(1) Another program may be editing the same file.  If this is the case,
+    be careful not to end up with two different instances of the same
+    file when making changes.  Quit, or continue with caution.
+(2) An edit session for this file crashed.
+    If this is the case, use ":recover" or "vim -r /home/kali/TBomb2.0/TBomb2.0.
+sh"
+    to recover the changes (see ":help recovery").
+    If you did this already, delete the swap file "/home/kali/TBomb2.0/.TBomb2.0
+.sh.swp"
+    to avoid this message.
+
+Swap 
