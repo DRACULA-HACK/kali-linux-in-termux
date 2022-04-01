bdc3a998fffc992223b288e663581f8b8e65e3e5
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-04-01 08:31:15 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,1 +1,17 @@
-1
+Kali linux apt update && apt upgrade -y
+
+pkg install fish -y
+
+termux-setup-storage
+
+pkg install wget -y
+
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+
+chmod +x install-nethunter-termux
+
+./install-nethunter-termux
+
+nh
+
+kex passwd
