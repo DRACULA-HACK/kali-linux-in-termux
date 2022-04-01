128605d68e85b075144daba88c622aac91c5cc6d
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-04-01 17:21:53 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -11,25 +11,6 @@ Cyan="\033[1;36m"        # Cyan
 White="\033[1;37m"       # White
 
 
-    clear
-    echo -e "\e[1;31m"
-    if ! [ -x "$(command -v figlet)" ]; then
-        echo 'Introducing TBomb'
-    else
-                 figlet kali-linux-in-termux
-    fi
-    if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Was Created By \e[1;32mMaster Hacke . dracula-hack \e[0m"
-    else
-        echo -e "\e[1;34mCreated By \e[1;34m"
-        figlet Master-hack
-    printf "\033[1;93m[\033[1;77m::\033[1;93m]	    \033[1;92m   Code by : \033[1;97m   master-hack	    \033[1;93m[\033[1;77m::\033[1;93m]"
-     printf "\n\033[1;93m[\033[1;77m::\033[1;93m]	\033[1;92mcontact number : \033[1;97m +91 6235369260 \033[1;93m[\033[1;77m::\033[1;93m]\n"
-    echo " "
-    echo " "
-    echo -e
-
-
 Kali linux apt update && apt upgrade -y
 
 pkg install fish -y
