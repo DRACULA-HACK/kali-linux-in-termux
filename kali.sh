56d5863ca9d95fdc059a0a0133d499b5160d1439
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-04-01 16:22:56 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,3 +1,33 @@
+#!/bin/bash
+
+# colour
+Black="\033[1;30m"       # Black
+Red="\033[1;31m"         # Red
+Green="\033[1;32m"       # Green
+Yellow="\033[1;33m"      # Yellow
+Blue="\033[1;34m"        # Blue
+Purple="\033[1;35m"      # Purple
+Cyan="\033[1;36m"        # Cyan
+White="\033[1;37m"       # White
+
+clear
+apt-get update
+apt-get upgrade
+
+    echo -e " \e[96m
+                                     ████████ \e[0m"
+echo "                                 ████████████████
+                                   █─▄▀█──█▀▄─█
+                                  ▐▌──────────▐▌"
+echo -e    "\e[0m                                  █▌\e[91m▀▄──▄▄──▄▀\e[0m▐█"
+echo -e    "\e[0m                                 ▐██──\e[91m▀▀\e[91m──▀▀──\e[0m██"
+echo "                                ▄████▄──▐▌──▄████▄"
+echo -e    "\e[91m"
+echo " "
+
+
+
+
 Kali linux apt update && apt upgrade -y
 
 pkg install fish -y
